81a8071d481257da4d97b75fd85a0dd156106f21
byronwasti <byron.wasti@gmail.com>
2015-09-19 18:02:06 -0400

    Moving around files

---
+++ extract_quotes.py
@@ -0,0 +1,51 @@
+import os
+
+def check_for_quotes(line):
+
+    count = line.count('"')
+
+    if count == 0:
+        return None
+    elif count % 2 != 0:
+        raise Exception("Quotation marks are not even; error in parsing quotes")
+    
+    locations = [i for i, ltr in enumerate(line) if ltr == '"']
+
+    quote_object = [ line[locations[i]:locations[i+1]+1] for i in xrange(0,count,2) ]
+
+    return quote_object
+
+
+def getQuotes( text ):
+    quotes = {}
+    potential = []
+
+    for line in text:
+        if 'said' in line or 'says' in line or 'told' in line:
+            potential.append(line)
+
+    for line in potential:
+        tmp = check_for_quotes(line)
+        if tmp != None:
+            quotes[line] = tmp
+
+    return quotes
+
+if __name__ == '__main__':
+    directory = 'ArticleTest/'
+    #articles = ['cnn.txt','guard.txt','huff.txt', 'ny.txt']
+    articles = ['guard.txt','huff.txt', 'ny.txt']
+    articles = [ os.path.join(directory, i) for i in articles]
+    
+    master = {}
+    master['articles'] = []
+
+    for article in articles:
+        article_dict = {}
+        article_dict['name'] = article
+        with open(article, 'r') as f:
+            article_dict['quotes'] = getQuotes( f )
+
+        master['articles'].append(article_dict)
+
+    print master['articles']
---
+++ genTxts.py
@@ -0,0 +1,34 @@
+from newspaper import Article
+import unicodedata
+import os
+
+def get_text(filename, directory):
+    url = urls[filename]
+    a = Article(url)
+    a.download()
+    a.parse()
+
+    text = a.text
+
+    text.replace(u'\u201d', u'"')
+    text.replace(u'\u201c', u'"')
+
+    return text.split('\n\n')
+
+
+if __name__ == '__main__':
+    urls = {"cnn.txt":"http://www.cnn.com/2015/09/16/us/texas-student-ahmed-muslim-clock-bomb/", 
+            "guard.txt":"http://www.theguardian.com/us-news/2015/sep/16/homemade-clock-ahmed-mohamed-texas-officials-we-were-right",
+            "huff.txt":"http://www.theguardian.com/commentisfree/2015/sep/16/ahmed-mohamed-clock-bigotry-american-muslims",
+            "ny.txt":"http://www.nytimes.com/2015/09/19/us/irving-police-chief-defends-response-to-ahmed-mohameds-clock.html"
+            }
+
+    for filename in urls:
+        print filename
+        text = get_text(filename, 'ArticleTest')
+        print text
+        print len(text)
+        exit()
+
+        with open(os.path.join(directory,filename), 'w') as output:
+            output.write(text)
