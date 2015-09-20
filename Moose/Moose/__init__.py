e693e11b42f4ae942c5603e9ca3d8db2dc82ce6b
Patrick Huston <patrickhuston1@gmail.com>
2015-09-19 21:23:32 -0400

    Fixes index route

--- Moose/Moose/__init__.py
+++ Moose/Moose/__init__.py
@@ -4,8 +4,8 @@ from flask import render_template
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return render_template('index.html', name=name)
+def index():
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run()
