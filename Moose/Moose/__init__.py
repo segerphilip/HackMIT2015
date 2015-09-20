8527c534ceb0c96c44c847487f5e3bf8c2f9a8ba
Philip Seger <seger.philip@gmail.com>
2015-09-20 01:36:44 -0400

    taking input from queries

--- Moose/Moose/__init__.py
+++ Moose/Moose/__init__.py
@@ -7,5 +7,10 @@ app = Flask(__name__)
 def start():
   return render_template('start.html')
 
+@app.route('/test')
+def article(link=['www.test.com', 'www.wow.doge', 'www.space.me'], articleTitle=['Formal Verification in Voss', 'So much Wow', 'Where is me?'], facts=['This is for chips.', 'Much text, so doge, wow.', 'Here.']):
+  return render_template('index.html', link=link, articleTitle=articleTitle, facts=facts)
+
 if __name__ == '__main__':
-    app.run()
+  app.debug = True
+  app.run()
