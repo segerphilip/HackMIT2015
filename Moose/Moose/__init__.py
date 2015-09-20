ba5e132d24ef9d8d0bfd6bc30a9df279f20dbcb8
byronwasti <byron.wasti@gmail.com>
2015-09-20 04:16:01 -0400

    Search button is functional

--- Moose/Moose/__init__.py
+++ Moose/Moose/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask import render_template
+from flask import request
 
 app = Flask(__name__)
 
@@ -7,10 +8,22 @@ app = Flask(__name__)
 def start():
   return render_template('start.html')
 
+@app.route('/', methods=['POST'])
+def requested_query():
+    if request.method == 'POST':
+        text = request.form['search']
+        if text == '':
+            return start()
+    
+        return article(text)
+
+    return start()
+
 @app.route('/test')
-def article(link=['www.test.com', 'www.wow.doge', 'www.space.me'], articleTitle=['Formal Verification in Voss', 'So much Wow', 'Where is me?'], facts=['This is for chips.', 'Much text, so doge, wow.', 'Here.']):
-  return render_template('index.html', link=link, articleTitle=articleTitle, facts=facts)
+def article(query):
+    print query
+    return render_template('index.html', link=link, articleTitle=articleTitle, facts=facts)
 
 if __name__ == '__main__':
-  app.debug = True
-  app.run()
+    app.debug = True
+    app.run()
