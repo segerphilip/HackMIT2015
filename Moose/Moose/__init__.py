bcc2dd790e2c0ec4fda13331eaa4a95f801d9a7e
Philip Seger <seger.philip@gmail.com>
2015-09-20 02:23:33 -0400

    working on integrations

--- Moose/Moose/__init__.py
+++ Moose/Moose/__init__.py
@@ -1,16 +1,19 @@
 from flask import Flask
 from flask import render_template
+import models.query
 
 app = Flask(__name__)
 
+Article = models.query
+
 @app.route('/')
 def start():
-  return render_template('start.html')
+    return render_template('start.html')
 
 @app.route('/test')
-def article(link=['www.test.com', 'www.wow.doge', 'www.space.me'], articleTitle=['Formal Verification in Voss', 'So much Wow', 'Where is me?'], facts=['This is for chips.', 'Much text, so doge, wow.', 'Here.']):
-  return render_template('index.html', link=link, articleTitle=articleTitle, facts=facts)
+def article(Article):
+    return render_template('index.html', Article=Article)
 
 if __name__ == '__main__':
-  app.debug = True
-  app.run()
+    app.debug = True
+    app.run()
