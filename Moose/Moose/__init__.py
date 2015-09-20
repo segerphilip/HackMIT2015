6153247a507acd4f8bdbc9f4b9c16377a8ef1245
Philip Seger <seger.philip@gmail.com>
2015-09-20 02:42:08 -0400

    working on adding to html

--- Moose/Moose/__init__.py
+++ Moose/Moose/__init__.py
@@ -1,18 +1,19 @@
 from flask import Flask
 from flask import render_template
-import models.query
+from models.query import Query
 
 app = Flask(__name__)
-
-Article = models.query
 
 @app.route('/')
 def start():
     return render_template('start.html')
 
 @app.route('/test')
-def article(Article):
-    return render_template('index.html', Article=Article)
+def article():
+    query = Query('')
+    # facts, title, url, sentiment, political, summary
+    query.create_fake()
+    return render_template('index.html', facts=query.facts, title=query.facts, url=query.url, sentiment=query.url, political=query.url, summary=query.url)
 
 if __name__ == '__main__':
     app.debug = True
