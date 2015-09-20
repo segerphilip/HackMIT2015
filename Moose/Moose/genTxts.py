badc6000d73e92760bbf2ca18d5606e8385569c9
byronwasti <byron.wasti@gmail.com>
2015-09-19 20:22:06 -0400

    Added u2018 swap to '

--- Moose/Moose/genTxts.py
+++ Moose/Moose/genTxts.py
@@ -14,6 +14,7 @@ def get_text(filename, directory):
     text = text.replace('\\u201d', '"')
     text = text.replace('\\u201c', '"')
     text = text.replace('\\u2019', '\'')
+    text = text.replace('\\u2018', '\'')
 
     return text#.split('\n\n')
 
